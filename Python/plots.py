b297e38c065e5b8efc1b4c35aeedaf5845978d75
ohlind <david.ohlin@yahoo.se>
2020-11-16 15:22:42 +0100

    Plotter file

---
+++ Python/plots.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+
+def show_loss(history):
+    fig = plt.figure(1)
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(loss) + 1)
+
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    fig.show()
+
+def show_accuracy(history):
+    fig = plt.figure(2)
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    fig.show()
