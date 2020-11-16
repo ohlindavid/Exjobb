2da70dd835ff5bb74e7285b88326e8e0f51834d5
ohlind <david.ohlin@yahoo.se>
2020-11-16 14:37:07 +0100

    Morlet layer finished

--- Python/MorletLayer.py
+++ Python/MorletLayer.py
@@ -6,13 +6,13 @@ import numpy as np
 class MorletConv(keras.layers.Layer):
     def __init__(self, input_dim):
         super(MorletConv, self).__init__()
-        self.chans = input_dim[0] #Antal kanaler
-        self.ttot = input_dim[1] #Tiden per trial
+        self.chans = input_dim[1] #Antal kanaler
+        self.ttot = input_dim[0] #Tiden per trial
         self.wlen = 25 #Fönsterbredd, från Zhao19
         self.etas = 25 #Antal fönster
 
-        self.a = self.add_weight(shape=(self.etas), initializer="random_normal", trainable=True)
-        self.b = self.add_weight(shape=(self.etas), initializer="random_normal", trainable=True)
+        self.a = self.add_weight(shape=(self.etas), initializer=keras.initializers.RandomNormal(mean=0.0, stddev=50.0), trainable=True)
+        self.b = self.add_weight(shape=(self.etas), initializer=keras.initializers.RandomNormal(mean=0.0, stddev=50.0), trainable=True)
 
     def call(self, inputs):
         output = np.zeros(self.ttot - (self.wlen - 1), self.etas, self.chans)
@@ -20,7 +20,8 @@ class MorletConv(keras.layers.Layer):
             morlet = lambda t: math.exp(-(self.a[eta]^2)*(t^2)/2)*math.cos(2*math.pi*self.b[eta]*t)
             window = morlet(range(self.wlen))
             for chan in range(self.chans):
-                np.convolve(inputs[])
+                output[:,eta,chan] = np.convolve(inputs[:,1], window)
+        return output
 
 
         #return tf.matmul(inputs, self.w) + self.b
