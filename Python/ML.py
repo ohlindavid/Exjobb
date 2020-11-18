0d90a863ac6f7b0f44ffcf6f668e44063c8d5f60
oKeding <oskar.keding@gmail.com>
2020-11-18 13:59:26 +0100

    fef

--- Python/ML.py
+++ Python/ML.py
@@ -27,20 +27,19 @@ model.add(MorletConv([L,nchan]))
 model.add(layers.Conv2D(filters=1, kernel_size=[nchan,1], activation='elu')) #kernel_size specificerar spatial faltning enligt Zhao19
 
 #Resten av nätverket
-model.add(layers.AveragePooling2D())
-model.add(layers.Dropout(0.75))
-model.add(layers.Dense(2, activation='softmax')) #2 klasser
-
+#model.add(layers.AveragePooling2D())
+#model.add(layers.Dropout(0.75))
+model.add(layers.Dense(2, activation='softmax')) #2
 model.compile(
     loss=losses.BinaryCrossentropy(),
     optimizer=optimizers.Adam(),
     metrics=['accuracy'],
     run_eagerly = True
 )
+
 #plot_model(model, to_file=dot_img_file, show_shapes=True)
-
 history = model.fit(data_generator,steps_per_epoch=20,epochs=4)
-
+model.summary()
 
 show_loss(history)
 show_accuracy(history)
--- Python/MorletLayer.py
+++ Python/MorletLayer.py
@@ -5,12 +5,12 @@ import numpy as np
 import math
 
 class MorletConv(keras.layers.Layer):
-    def __init__(self, input_dim):
+    def __init__(self, input_dim,input_shape=[1024,25,1]):
         super(MorletConv, self).__init__()
         self.nchan = input_dim[1] #Antal kanaler
         self.ttot = input_dim[0] #Tiden per trial
         self.wlen = 25 #Fönsterbredd i samples, från Zhao19
-        self.etas = 25 #Antal fönster
+        self.etas = 1 #Antal fönster
         self.wtime = 0.36 #Fönsterbredd i tid
 
         self.a = self.add_weight(name='a', shape=(self.etas,1), initializer=keras.initializers.RandomNormal(mean=0.0, stddev=10.0), trainable=True)
@@ -22,11 +22,11 @@ class MorletConv(keras.layers.Layer):
         twin = tf.tile(tf.map_fn(morlet, win), tf.constant([1,1,self.nchan]))
         tinput = tf.tile(tf.expand_dims(np.transpose(inputs), axis=1), tf.constant([1,self.etas,1]))
         tinput = tf.expand_dims(tinput,axis=0)
-#        tinput = tf.expand_dims(tinput,axis=-1)
-#        twin = tf.expand_dims(twin, axis=-1)
-        twin = tf.expand_dims(twin, axis=-1)
+#       tinput = tf.expand_dims(tinput,axis=-1)
+        twin = tf.tile(tf.expand_dims(twin, axis=-1),tf.constant([1,1,1,1]))
+        #twin = tf.tile(tf.expand_dims(twin, axis=-1),tf.constant([1,1,1,1, 1]))
         print(tf.shape(twin))
         print(tf.shape(tinput))
         output = tf.nn.convolution(tinput,twin, padding='VALID')
-        print(tf.shape(output))
+        #print(output)
         return output
