c94d0ca75a6c540a001e7c34e43ee4035860cc73
oKeding <oskar.keding@gmail.com>
2020-11-18 11:41:17 +0100

    fix to ML and moretLayer

--- Python/MorletLayer.py
+++ Python/MorletLayer.py
@@ -7,7 +7,7 @@ import math
 class MorletConv(keras.layers.Layer):
     def __init__(self, input_dim):
         super(MorletConv, self).__init__()
-        self.chans = input_dim[1] #Antal kanaler
+        self.nchan = input_dim[1] #Antal kanaler
         self.ttot = input_dim[0] #Tiden per trial
         self.wlen = 25 #Fönsterbredd i samples, från Zhao19
         self.etas = 25 #Antal fönster
@@ -17,6 +17,17 @@ class MorletConv(keras.layers.Layer):
         self.b = self.add_weight(name='b', shape=(self.etas,1), initializer=keras.initializers.RandomNormal(mean=0.0, stddev=10.0), trainable=True)
 
 #GAMMAL
+
+
+    def call(self, inputs):
+        morlet = lambda t: tf.math.exp(-(tf.math.pow(self.a,2))*(tf.math.pow(t,2))/2)*tf.math.cos(tf.constant(2*math.pi)*self.b*t)
+        win = tf.constant(np.linspace(-self.wtime/2,self.wtime/2,self.wlen,dtype='float32'))
+        print(tf.map_fn(morlet, win))
+        twin = tf.tile(tf.map_fn(morlet, win), tf.constant([1,1,self.nchan]))
+        tinput = tf.tile(tf.expand_dims(inputs, axis=1), tf.constant([1,self.etas,1]))
+        tf.nn.convolution(tinput, twin, padding='VALID')
+        return
+
 """
     def call(self, inputs):
         output = np.zeros((self.ttot - (self.wlen - 1), self.etas, self.chans))
@@ -28,10 +39,3 @@ class MorletConv(keras.layers.Layer):
         output = tf.expand_dims(output, 0)
         return output
 """
-
-    def call(self, inputs):
-        morlet = lambda t: tf.math.exp(-(tf.math.pow(self.a,2))*(tf.math.pow(t,2))/2)*tf.math.cos(tf.constant(2*math.pi)*self.b*t)
-        win = tf.constant(np.linspace(-self.wtime/2,self.wtime/2,self.wlen,dtype='float32'))
-        twin = tf.tile(tf.expand_dims(tf.map_fn(morlet, win), axis=-1), tf.constant([1,1,self.nchan]))
-        tinput = tf.tile(tf.expand_dims(inputs, axis=1), tf.constant([1,self.etas,1]))
-        tf.nn.convolution(inputs, twin, padding='VALID')
