d485b95f46c483a504c4fbdced47ffedcd726222
oKeding <oskar.keding@gmail.com>
2020-11-27 09:43:26 +0100

    Loads of test simulations

--- sim/MATLAB/Simulera indata/generatingnoise.m
+++ sim/MATLAB/Simulera indata/generatingnoise.m
@@ -3,10 +3,10 @@ close all;
 clc;
 E = 60;
 time = 4;
-N = 1024;
+Fs = 69.4;
+N = 140;
 M = N/2;
 noreal = 10;
-Fs = 256;
 
 for j=1:100
 
@@ -30,7 +30,7 @@ for j=1:100
        alpha_noise(:,i)=filter(B,A,alpha_wn1(:,i));
        alpha_noise(:,i)=alpha_noise(:,i)./sqrt(alpha_noise(:,i)'*alpha_noise(:,i))*sqrt(N); % Normalized to power one
     end
-
+    
     % Simulate pink noise
     x = pinknoise(N,E)*20;
     y = fft(x);
@@ -48,17 +48,32 @@ for j=1:100
 
 
     tot_noise = w_noise(1)*alpha_noise + w_noise(2)*measurement_noise + w_noise(3)*oof_noise;
+    
 
-
-    in = linspace(0,time,N);
-    for i=1:5
-        z = 0.1*rand();
-     %   in2(:,i) =  exp(-1/5*in).*sin(3*in+2+z) + exp(-10*in).*sin(10*in) - 5*sin(12*in-pi/5+z).*exp(-2*in);
-        in2(:,i) = exp(-2*in).*sin(8*in+2) + exp(-in).*sin(2*in+z) - 5*sin(2*in-pi/0.8+z).*exp(-2*in);
-    end
-    in = in2;
-    SNR=20;
-    lambda=10^(SNR/10);
-    out = sqrt(lambda/(lambda+1)).*in./norm(in)+1/sqrt(lambda+1)*tot_noise(:,5)/norm(tot_noise(:,5));
-    csvwrite('sim_test_2ch_' + string(j),out)
+   % in = linspace(0,time,N);
+   % for i=1:5
+   %     z = 0.1*rand();
+   %    in2(:,i) =  exp(-1/5*in).*sin(3*in+2+z) + exp(-10*in).*sin(10*in) - 5*sin(12*in-pi/5+z).*exp(-2*in);
+   %     in2(:,i) = exp(-2*in).*sin(8*in+2) + exp(-in).*sin(2*in+z) - 5*sin(2*in-pi/0.8+z).*exp(-2*in);
+   % end
+   % in = in2;
+   rand(noreal,1)*2*pi
+   for  i=1:2
+    [X,T] = multigaussdata(N,[512],[1],[1],[7],[0],Fs);
+    [X2,T2] = multigaussdata(N,[512],[1],[1],[7],[0],Fs);
+    %[X,T] = multigaussdata(N,[512, 100, 200, 700, 50],[1/2,1/2,1/2,1/2],[1,1,0.5,2],[4,12,8,17],rand(noreal,4)*2*pi,Fs);
+    %[X2,T2]= multigaussdata(N,[512,60,150,650],[1/2,1/2,1/2,1/2],[1,0.5,2,0.2],[5,4,17,13],rand(noreal,4)*2*pi,Fs);
+    Ain(:,i) = X(:,1);
+    Bin(:,i) = 0.1*X2(:,1);
+   end
+   SNR=20;
+   lambda=10^(SNR/10);
+   lambda2 = 10^(SNR/10/17);
+   Aout = sqrt(lambda/(lambda+1)).*Ain./norm(Ain)+1/sqrt(lambda+1)*tot_noise(:,5)/norm(tot_noise(:,5));
+   Bout = sqrt(lambda2/(lambda2+1)).*Bin./norm(Bin)+1/sqrt(lambda+1)*tot_noise(:,5)/norm(tot_noise(:,5));
+   csvwrite('Asimulated_test_' + string(j),Ain);
+   csvwrite('Bsimulated_test_' + string(j),Bin);
 end
+plot(Ain)
+hold on;
+plot(Bin)
