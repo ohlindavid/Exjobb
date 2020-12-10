9414284c583a15acc50f6c288c994503607de6e4
ohlind <david.ohlin@yahoo.se>
2020-12-10 14:13:21 +0100

    First implementation of reassignment layer finished

--- Python/ReassignmentLayer.py
+++ Python/ReassignmentLayer.py
@@ -14,34 +14,74 @@ class ReassignmentSpec(keras.layers.Layer):
         self.lambda = 20 #Lambda från Sandsten18
         self.nfft = 2048 #Antal fft-samples
         self.epsilon = 0.1 #Epsilon att lägga till spektrogrammet för att förhindra division med noll
+        self.margin = 1000 #Margin to avoid out of bounds error when reassigning
+
         self.c = self.add_weight(name='c', shape=(self.sigmas,1), initializer=keras.initializers.RandomNormal(mean=20.0, stddev=4.0,seed=1), trainable=True)
 
     def call(self, inputs):
-
+        #Dimensions
         hl = self.lamda*12
         data = tf.real(inputs)
-        rows = tf.size(data)
+        rows = tf.size(data) + 1
         cols = tf.size(data) + hl
 
-
-        #Window function
-        H = np.exp(-0.5*np.square(np.arange(-hl/2, hl/2-1,1)/self.lambda))
+        #Window functions (H, dH and tH)
+        H = np.exp(-0.5*np.square(np.arange(-hl/2, hl/2,1)/self.lambda))
         dH = (np.pad(H, (0,2)) - np.pad(H, (2,0)))/2
-        dH[0] = 0
         dH[1] = 0
-        dH[-2] = 0
         dH[-1] = 0
-        TH = np.arange(-l/2+1, l/2)*H
+        dH = dH[1:-1]
+        tH = np.arange(-l/2+1, l/2)*H
 
-        input = tf.pad(input, [[0, self.ttot-self.fftpad]])
+        #Matrix representations
+        data = tf.pad(data, [hl/2,hl/2])
+        data = tf.repeat(data, rows) #Detta är nog fel, orkar inte fixa nu
+        Hmat = np.zeros((rows, cols))
+        dHmat = np.zeros((rows, cols))
+        tHmat = np.zeros((rows, cols))
+        M = np.zeros((rows, cols))
+        for i=0:rows-1
+            M[i,:] = np.pad(np.ones((1, hl), (i-1,cols-i-hl+1))
+            Hmat[i,:] =  np.pad(H, (i-1,cols-i-hl+1))
+            dHmat[i,:] = np.pad(dH, (i-1,cols-i-hl+1))
+            tHmat[i,:] = np.pad(tH, (i-1,cols-i-hl+1))
 
-        Fh = tf.signal.fft(tf.nn.conv2d(inputs,H))
-        Fth = tf.signal.fft(tf.nn.conv2d(inputs,TH))
-        Fdh = tf.signal.fft(tf.nn.conv2d(inputs,dH))
+        data = data*M
+        data = data - tf.linalg.matmul(data, tf.ones((cols)))*M/hl
+        fH = data*H
+        fdH = data*dH
+        ftH = data*tH
+        FH = tf.signal.fft(fH)
+        FdH = tf.signal.fft(fdH)
+        FtH = tf.signal.fft(ftH)
 
         #Gaussian windowed spectrogram
         SS = tf.math.square(tf.math.abs(Fh))
+        SS = SS + self.epsilon #Add epsilon to avoid division by zero later
 
         #Displacement vectors
         ct = (tf.square(self.lambda) + tf.square(self.sigma))/tf.square(self.lambda)
         cw = (tf.square(self.lambda) + tf.square(self.sigma))/tf.square(self.sigma)
+
+        #Displacement matrices
+        jj0 = ct*tf.math.real(Fth*tf.math.conj(Fh)/ss)
+        ii0 = cw*self.nfft/(2*np.pi)*tf.math.imag(Fdh.*tf.math.conj(Fh)/ss)
+        ii = np.transpose(np.arange(1, nfft/2, 1))
+        jj = np.transpose(np.arange(1, rows, 1))
+        ii = tf.repeat(ii, [1, rows])
+        jj = tf.repeat(jj, [nfft/2, 1])
+        jj = jj + tf.math.round(jj0)
+        ii = ii + tf.math.round(ii0)
+
+        #Margins to avoid out of bounds error when reassigning
+        xs = (self.nfft/2) + 2*self.margin
+        ys = (rows-1) + 2*self.margin
+        rss = np.zeros((xs, ys));
+
+        #Reassignment
+        for m = 1:nfft/2
+            for n = 1:(rows-1)
+                rss[self.margin + ii[m,n], self.margin + jj[m,n]] = rss[self.margin + ii[m,n], self.margin + jj[m,n]) + SS[m,n]
+
+        #Cutting margins
+        rss = tf.slice(rss, [self.margin,self.margin], [xs-self.margin-1,ys-self.margin-1])
