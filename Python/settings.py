a21fd0830e77cb6ff53e0371ca492042b33dae77
ohlind <david.ohlin@yahoo.se>
2020-12-16 17:09:10 +0100

    who?

--- Python/settings.py
+++ Python/settings.py
@@ -1,15 +1,5 @@
 #User
-who = "Oskar"
-def path():
-    if who=="Oskar":
-        return "C:/Users/Oskar/Documents/GitHub/exjobb/AD_comp_to_Bramao/"
-    if who=="David":
-        return "C:/Users/david/Documents/GitHub/exjobb/sim/test_sim_1ch/"
-def pathPred():
-    if who=="Oskar":
-        return "C:/Users/Oskar/Documents/GitHub/exjobb/AD_comp_to_Bramao_Pred/"
-    if who=="David":
-        return "C:/Users/david/Documents/GitHub/exjobb/sim/test_sim_1ch/"
+who = "David"
 
 #Run
 epochs = 75
