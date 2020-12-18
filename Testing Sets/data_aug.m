fcad4ac997e4b2ab40b80f28f0f2fe7066a26259
oKeding <oskar.keding@gmail.com>
2020-12-18 10:46:56 +0100

    Split data and Augment data

--- Testing Sets/data_aug.m
+++ Testing Sets/data_aug.m
@@ -1,57 +1,104 @@
+% TRAIN,VALIDATION,TEST SPLIT
+
 folder = "C:\Users\Oskar\Documents\GitHub\Exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_6";
-aim_folder = folder + "\augmented_data\";
+aim_folder_train = folder + "\augmented_data_train\";
+aim_folder_val = folder + "\augmented_data_val\";
+aim_folder_test = folder + "\data_test\";
 direc = dir(folder);
 direc = direc(3:end);
-mkdir(aim_folder);
-for file = {direc.name}
-    data = load(folder+"\"+file{1});
-    nchan = size(data,2);
-    length = size(data,1);
+mkdir(aim_folder_train);
+mkdir(aim_folder_val)
+mkdir(aim_folder_test);
+direc = vertcat({direc.name});
+direc = direc(randperm(length(direc)));
+direcOriginal = (direc);
+for class = ['A','B','C']
+    i = 1;
+    direc = {};
+    for element=direcOriginal
+        if regexp(element{1}, regexptranslate('wildcard',class+"*"))
+            direc{i} = element;
+            i=i+1;
+        end
+    end
     
-    % Subtract mean.
-    the_mean = mean(data,1);
-    data = data - the_mean;
- 
-    % Normalize/standardize signals (maybe mean of sum of abs?) 
-    mean_of_sum_of_abs = mean(mean(abs(data),1));
-    data = data/mean_of_sum_of_abs; 
+    for file = direc(1:floor((4/5)^2*length(direc)))
+        data = load(folder+"\"+file{1});
+        nchan = size(data,2);
+        len = size(data,1);
+
+        % Subtract mean.
+        the_mean = mean(data,1);    
+        data = data - the_mean;
+
+        % Normalize/standardize signals (maybe mean of sum of abs?) 
+        mean_of_sum_of_abs = mean(sqrt(var(data,1)));
+        data = data/mean_of_sum_of_abs; 
+
+        % Save to folder.
+        csvwrite(aim_folder_train+file,data); 
+    end
+
+    for file = direc(1+floor((4/5)^2*length(direc)):floor(4/5*length(direc)))
+        data = load(folder+"\"+file{1});
+        nchan = size(data,2);
+        len = size(data,1);
+
+        % Subtract mean.
+        the_mean = mean(data,1);    
+        data = data - the_mean;
+
+        % Normalize/standardize signals (maybe mean of sum of abs?) 
+        mean_of_sum_of_abs = mean(sqrt(var(data,1)));
+        data = data/mean_of_sum_of_abs; 
+
+        % Save to folder.
+        csvwrite(aim_folder_val+file,data); 
+    end
+
+    for file = direc(1+floor(4/5*length(direc)):end)
+        data = load(folder+"\"+file{1});
+        nchan = size(data,2);
+        len = size(data,1);
+
+        % Subtract mean.
+        the_mean = mean(data,1);    
+        data = data - the_mean;
+
+        % Normalize/standardize signals (maybe mean of sum of abs?) 
+        mean_of_sum_of_abs = mean(sqrt(var(data,1)));
+        data = data/mean_of_sum_of_abs; 
+
+        % Save to folder.
+        csvwrite(aim_folder_test+file,data); 
+    end
+end
+%% DATA AUGMENTATION.
+
+for aim_folder = [aim_folder_train]
     
-    % Save to folder.
-    csvwrite(aim_folder+file,data); 
+    folder = aim_folder;
+    direc = dir(folder);
+    direc = direc(3:end);
+    direc = vertcat({direc.name});
+    direc = direc(randperm(length(direc)));
+
+    for file = direc
+        % Invert Signal.
+        data = load(folder+"\"+file{1});
+        csvwrite(aim_folder+file+"_neg",-data);
+    end
+
+    direc = dir(folder);
+    direc = direc(3:end);
+    direc = vertcat({direc.name});
+    direc = direc(randperm(length(direc)));
+    
+    for file = direc
+        % Noise.
+        data = load(folder+"\"+file{1});
+        noise = 0.01*randn(len,nchan);
+        data_noise = data+noise;
+        csvwrite(aim_folder+file+"_noise1",data_noise); 
+    end
 end
-
-folder = aim_folder;
-direc = dir(folder);
-direc = direc(3:end);
-
-for file = {direc.name}
-    % Invert Signal.
-    data = load(folder+"\"+file{1});
-    csvwrite(aim_folder+file+"_neg",-data);
-end
-
-direc = dir(folder);
-direc = direc(3:end);
-
-for file = {direc.name}
-    % Noise.
-    data = load(folder+"\"+file{1});
-    noise = 0.001*randn(length,nchan);
-    data_noise = data+noise;
-    csvwrite(aim_folder+file+"_noise1",data_noise); 
-end
-
-direc = dir(folder);
-direc = direc(3:end);
-
-for file = {direc.name}
-    % Noise.
-    data = load(folder+"\"+file{1});
-    noise = 0.001*randn(length,nchan);
-    data_noise = data+noise;
-    csvwrite(aim_folder+file+"_noise2",data_noise); 
-end
-
-direc = dir(folder);
-direc = direc(3:end);
-
--- Testing Sets/generateFromAD_Whole.m
+++ Testing Sets/generateFromAD_Whole.m
@@ -1,13 +1,13 @@
 time = 4;
 bin_size = 4;
 number_bins = round(time/bin_size);
-v_len = length(Subj06_CleanData_study_FA.trial{1});
+v_len = length(Subj02_CleanData_study_FA.trial{1});
 onset = floor(1.5/4*v_len);
 
 for i=1:60
-    signalA = Subj06_CleanData_study_FA.trial{i}';
-    signalB = Subj06_CleanData_study_LM.trial{i}';
-    signalC = Subj06_CleanData_study_OB.trial{i}';
+    signalA = Subj02_CleanData_study_FA.trial{i}';
+    signalB = Subj02_CleanData_study_LM.trial{i}';
+    signalC = Subj02_CleanData_study_OB.trial{i}';
     for j=0:number_bins-1
         low_index = j*floor(v_len/number_bins)+1;
         high_index = (j+1)*floor(v_len/number_bins);
@@ -15,21 +15,21 @@ for i=1:60
             break;
             high_index = v_len;
         end
-        sig_2_write_A = signalA(onset:high_index,25:30);
-        sig_2_write_B = signalB(onset:high_index,25:30);
-        sig_2_write_C = signalC(onset:high_index,25:30);
-        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_6\A' + string(i),sig_2_write_A);
-        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_6\B' + string(i),sig_2_write_B);
-        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_6\C' + string(i),sig_2_write_C);
+        sig_2_write_A = signalA(onset:high_index,:);
+        sig_2_write_B = signalB(onset:high_index,:);
+        sig_2_write_C = signalC(onset:high_index,:);
+        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_2\A' + string(i),sig_2_write_A);
+        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_2\B' + string(i),sig_2_write_B);
+        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_2\C' + string(i),sig_2_write_C);
     end
 end
 
-v_len = length(Subj06_CleanData_test_FA_lexical.trial{1});
+v_len = length(Subj02_CleanData_test_FA_lexical.trial{1});
 
-for i=1:29
-    signalA = Subj06_CleanData_test_FA_lexical.trial{i}';
-    signalB = Subj06_CleanData_test_LM_lexical.trial{i}';
-    signalC = Subj06_CleanData_test_OB_lexical.trial{i}';
+for i=1:28
+    signalA = Subj02_CleanData_test_FA_lexical.trial{i}';
+    signalB = Subj02_CleanData_test_LM_lexical.trial{i}';
+    signalC = Subj02_CleanData_test_OB_lexical.trial{i}';
     for j=0:number_bins-1
         low_index = j*floor(v_len/number_bins)+1;
         high_index = (j+1)*floor(v_len/number_bins);
@@ -37,12 +37,12 @@ for i=1:29
             break;
             high_index = v_len;
         end
-        sig_2_write_A = signalA(onset:high_index,25:30);
-        sig_2_write_B = signalB(onset:high_index,25:30);
-        sig_2_write_C = signalC(onset:high_index,25:30);
-        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_6_pred\A' + string(i),sig_2_write_A);
-        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_6_pred\B' + string(i),sig_2_write_B);
-        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_6_pred\C' + string(i),sig_2_write_C);
+        sig_2_write_A = signalA(onset:high_index,:);
+        sig_2_write_B = signalB(onset:high_index,:);
+        sig_2_write_C = signalC(onset:high_index,:);
+        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_2_pred\A' + string(i),sig_2_write_A);
+        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_2_pred\B' + string(i),sig_2_write_B);
+        csvwrite('C:\Users\Oskar\Documents\GitHub\exjobb\Testing Sets\Albin&Damir\AD_data_set_subject_2_pred\C' + string(i),sig_2_write_C);
 
     end
 end
