c0b93bf47457b69119db9d84980cbfc48db7acb9
oKeding <oskar.keding@gmail.com>
2021-02-05 09:13:43 +0100

    update to handle channel indep in gradCAN

--- Python/define_model.py
+++ Python/define_model.py
@@ -25,11 +25,11 @@ def define_model_bins(nchan,L,Fs):
 def define_model(nchan,L,Fs):
     model = tf.keras.Sequential()
     model.add(layers.InputLayer((L,nchan),batch_size=1))
-    model.add(layers.LayerNormalization())
+    model.add(layers.LayerNormalization(axis=[1]))
     model.add(MorletConvRaw([L,nchan],Fs,input_shape=[L,nchan,1],etas=etas,wtime=wtime))
     model.add(layers.Conv2D(filters=filters, kernel_size=[1,nchan], activation='elu'))
     model.add(layers.Permute((3,1,2), name="second_permute"))
-    model.add(layers.AveragePooling2D(pool_size=(1,71), strides=(1,15)))
+    model.add(layers.AveragePooling2D(pool_size=(1,71), strides=(1,15),name="pooling"))
     model.add(layers.Dropout(0.75))
     model.add(layers.Flatten())
     model.add(layers.Dense(3))
